9d51008148b0f19ec57ae5190ca59490b3ba14c2
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-27 17:43:41 +0100

    add: cards

---
+++ src/components/AppCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    image: String,
+    name: String,
+    type: String,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img src="" alt="" />
+    <h4>CARTA</h4>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,11 +1,22 @@
 <script>
-export default {};
+import AppCard from "./AppCard.vue";
+import { store } from "../store.js";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { AppCard },
+};
 </script>
 
 <template>
   <div class="container">
     <div class="row">
-      <div class="col">pippo</div>
+      <div class="col" v-for="card in store.cardList">
+        <AppCard />
+      </div>
     </div>
   </div>
 </template>
@@ -18,5 +29,17 @@ export default {};
   background-color: white;
   min-height: 500px;
   padding: 3rem;
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+
+    .col {
+      width: calc(100% / 5 - 0.5rem);
+    }
+  }
 }
 </style>
