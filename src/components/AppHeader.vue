4c09b569e802b4926808eae521c1eea64c077ca7
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-27 17:04:23 +0100

    add: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+  <header>
+    <div class="header-top">
+      <div class="logo">
+        <img src="" alt="" />
+      </div>
+      <h2>API Yu-Gi-Oh</h2>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  height: 80px;
+  background-color: white;
+
+  .header-top {
+    height: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0 2rem;
+  }
+}
+</style>
